cf9addb061ec6040fb4f131a201c5905148b06f2
Miriam Rehema <rehemamiriam30@gmail.com>
2025-06-17 09:27:53 +0200

    Done with product table and model

--- app/Models/Products.php
+++ app/Models/Products.php
@@ -2,9 +2,46 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Products extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable=[
+        'name',
+        'slug',
+        'image',
+        'description',
+        'price',
+        'is_active',
+        'category_id',
+        'stock',
+        'is_featured',
+        'is_new',
+        'is_on_sale',
+        'sale_price',
+        
+
+
+    ];
+
+    protected $casts=[
+        'images'=> 'array'
+
+    ];
+
+     public function category(){
+        return $this->belongsTo(Category::class);
+    }
+     public function items(){
+        return $this->hasMany(Order_item::class);
+    }
+   
+     public function reviews(){
+        return $this->HasMany(Review::class);
+    }
+   
+    
 }

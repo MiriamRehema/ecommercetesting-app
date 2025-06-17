38d28d796a607d6666ae8d339eaab7fbec7c13a7
Miriam Rehema <rehemamiriam30@gmail.com>
2025-06-17 09:28:08 +0200

    Done with review table and model

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -3,8 +3,28 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\HasFactory;
 
 class Review extends Model
 {
-    //
+    use HasFactory;
+
+    private $fillable=[
+        'product_id',
+        'user_id',
+        'rating',
+        'comment',
+        
+
+    ];
+
+    
+     public function product(){
+        return $this->belongsTo(Product::class);
+    }
+     public function user(){
+    return $this->belongsTo(User::class);
+   }
+    
+    
 }
--- database/migrations/2025_06_16_093036_create_reviews_table.php
+++ database/migrations/2025_06_16_093036_create_reviews_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('reviews', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            $table->foreignId('product_id')->constrained('products')->cascadeOnDelete();
+            $table->string('rating')->nullable();
+            $table->longText('comment')->nullable();
             $table->timestamps();
         });
     }

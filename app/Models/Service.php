946b10cb3df740e6d0c2c1aa225f49956e649733
Miriam Rehema <rehemamiriam30@gmail.com>
2025-06-17 09:28:29 +0200

    Don with service model and table

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -3,8 +3,23 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\HasFactory;
 
 class Service extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable=[
+        'name',
+        'slug',
+        'description',
+        'image',
+        'is_active',
+        
+
+    ];
+    protected $casts=[
+        'images'=> 'array'
+
+    ];
 }

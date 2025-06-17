21c9513f8c35d6b7e6ecbbe1c17537ee25341326
Miriam Rehema <rehemamiriam30@gmail.com>
2025-06-17 09:27:36 +0200

    Done with order table and model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -1,10 +1,34 @@
 <?php
 
 namespace App\Models;
-
+use Illuminate\Database\Eloquent\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+   use HasFactory;
+
+   protected $fillable=[
+    'user_id',
+     'grand_total',
+     'payment_method',
+     'status',
+     'payment_status',
+     'currency',
+     'shipping_amount',
+     'shipping_method',
+     'notes',
+   
+   ];
+
+   public function user(){
+    return $this->belongsTo(User::class);
+   }
+   public function items(){
+    return $this->HasMany(Order_item::class);
+   }
+   public function adress(){
+    return $this->HasOne(Address::class);
+   }
+   
 }

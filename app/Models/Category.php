428fd774a0914d924a0f864d60cca2dfebeda2e3
Miriam Rehema <rehemamiriam30@gmail.com>
2025-06-18 09:15:54 +0200

    made some changes to category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,14 +2,14 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\HasFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
-    use Hasfactory;
+    
+    use HasFactory;
     protected $fillable=[
         'name',
         'slug',

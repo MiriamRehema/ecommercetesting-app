b023973bcff01d215e99ca9cde0c447719245bc3
Miriam Rehema <rehemamiriam30@gmail.com>
2025-06-19 09:24:49 +0200

    created the products resource

---
+++ app/Filament/Resources/ProductsResource.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ProductsResource\Pages;
+use App\Filament\Resources\ProductsResource\RelationManagers;
+use App\Models\Products;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Forms\Components\Group;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\TextArea;
+use Filament\Forms\Components\Toggle;
+use Filament\Forms\Set;
+use Filament\Tables\Actions\ActionGroup;
+use Filament\Tables\Actions\DeleteAction;
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Actions\ViewAction;
+use Filament\Tables\Actions\MarkdownEditer;
+
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ProductsResource extends Resource
+{
+    protected static ?string $model = Products::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Group::make()->schema([
+                        Section::make('Product Information')->schema([
+                            TextInput::make('name')
+                            ->required()
+                            ->maxLength(255),
+
+                            TextInput::make('slug')
+                            ->required()    
+                            ->maxLength(255)
+                            ->disabled(),
+
+                           
+
+
+
+
+
+                        ])->columns(2),
+                        
+                
+                ])->columnSpan(2),
+                
+            ])->columns(5);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('slug')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('price')
+                    ->money()
+                    ->sortable(),
+                Tables\Columns\IconColumn::make('is_active')
+                    ->boolean(),
+                Tables\Columns\TextColumn::make('category_id')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('stock')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\IconColumn::make('is_featured')
+                    ->boolean(),
+                Tables\Columns\IconColumn::make('is_new')
+                    ->boolean(),
+                Tables\Columns\IconColumn::make('is_on_sale')
+                    ->boolean(),
+                Tables\Columns\TextColumn::make('sale_price')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListProducts::route('/'),
+            'create' => Pages\CreateProducts::route('/create'),
+            'edit' => Pages\EditProducts::route('/{record}/edit'),
+        ];
+    }
+}

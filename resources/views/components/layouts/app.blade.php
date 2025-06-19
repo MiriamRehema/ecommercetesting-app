3c2f4301442cf1ace8bc26bbbaa3bb09405ab7a8
Miriam Rehema <rehemamiriam30@gmail.com>
2025-06-19 09:20:55 +0200

    added the  link for the icons

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -2,4 +2,7 @@
     <flux:main>
         {{ $slot }}
     </flux:main>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    
+
 </x-layouts.app.sidebar>

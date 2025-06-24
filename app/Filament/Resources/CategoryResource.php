354a091c73a4e1c9399b7b1b44840a423b8dd18e
Miriam Rehema <rehemamiriam30@gmail.com>
2025-06-24 09:45:08 +0200

    added the sort feature for display

--- app/Filament/Resources/CategoryResource.php
+++ app/Filament/Resources/CategoryResource.php
@@ -33,6 +33,8 @@ class CategoryResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-squares-2x2';
 
+    protected static ?int $navigationSort = 3;
+
     public static function form(Form $form): Form
     {
         return $form

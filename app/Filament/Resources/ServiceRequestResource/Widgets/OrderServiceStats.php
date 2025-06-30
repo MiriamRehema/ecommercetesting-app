e86127affca4a346fde4c8b8ffdd57d7e48f0f64
Miriam Rehema <rehemamiriam30@gmail.com>
2025-06-30 10:03:45 +0200

    created order service stats

---
+++ app/Filament/Resources/ServiceRequestResource/Widgets/OrderServiceStats.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Filament\Resources\ServiceRequestResource\Widgets;
+
+use Filament\Widgets\ChartWidget;
+use App\Models\Service_request;
+use App\Models\Service;
+use App\Models\Order_service;
+use Filament\Tables\Actions\ViewAction;
+use Filament\Tables\Actions\EditAction;
+
+class OrderServiceStats extends ChartWidget
+{
+    protected static ?string $heading = 'Chart';
+
+    protected function getData(): array
+    {
+        return [
+            'data' => Service_request::query()->count(),    
+            'datasets' => [
+                [
+                    'label' => 'Service Requests',
+                    'data' => \App\Models\Order_service::query()
+                        ->selectRaw('service_id, SUM(quantity) as total_quantity')
+                        ->groupBy('service_id')
+                        ->pluck('total_quantity')
+                        ->toArray(),
+                    'backgroundColor' => [
+                        '#FF6384',
+                        '#36A2EB',
+                        '#FFCE56',
+                        '#4BC0C0',
+                        '#9966FF',
+                    ],
+                ],
+            ],
+            'labels' => \App\Models\Order_service::query()
+                ->select('service_id')
+                ->distinct()
+                ->pluck('service_id')
+                ->map(function ($serviceId) {
+                    return \App\Models\Service::find($serviceId)->name ?? 'Unknown Service';
+                })
+                ->toArray(),
+                        
+        ];
+
+    }
+
+    protected function getType(): string
+    {
+        return 'doughnut';
+    }
+}

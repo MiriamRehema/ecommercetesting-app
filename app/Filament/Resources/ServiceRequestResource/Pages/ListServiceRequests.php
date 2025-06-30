b6ea8db3e656c78ef6ce7d7dd896b862ca811fef
Miriam Rehema <rehemamiriam30@gmail.com>
2025-06-30 10:04:02 +0200

    added the functionality of the stats

--- app/Filament/Resources/ServiceRequestResource/Pages/ListServiceRequests.php
+++ app/Filament/Resources/ServiceRequestResource/Pages/ListServiceRequests.php
@@ -4,6 +4,8 @@ namespace App\Filament\Resources\ServiceRequestResource\Pages;
 
 use App\Filament\Resources\ServiceRequestResource;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
+use App\Filament\Resources\ServiceRequestResource\Widgets\OrderServiceStats;
 use Filament\Resources\Pages\ListRecords;
 
 class ListServiceRequests extends ListRecords
@@ -15,5 +17,27 @@ class ListServiceRequests extends ListRecords
         return [
             Actions\CreateAction::make(),
         ];
+
     }
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            OrderServiceStats::class,
+        ];
+    }
+    public function getTabs(): array
+    {
+        return [
+            null=>Tab::make('All'),
+            'requested'=>Tab::make('Requested')
+                ->query(fn ( $query) => $query->where('status', 'requested')),
+            'in_progress'=>Tab::make('In Progress')
+                ->query(fn ( $query) => $query->where('status', 'in_progress')), 
+            'completed'=>Tab::make('Completed')
+                ->query(fn ( $query) => $query->where('status', 'completed')),
+            'cancelled'=>Tab::make('Cancelled')
+                ->query(fn ( $query) => $query->where('status', 'cancelled')),
+        ];
+    }
+   
 }

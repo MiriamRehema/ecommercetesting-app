806627065def8de933dae2ce9f9af5abc7f0453b
Miriam Rehema <rehemamiriam30@gmail.com>
2025-07-01 09:26:06 +0200

    added the order service stats to work

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 use App\Filament\Resources\OrderResource\Widgets\OrderStats;
+use App\Filament\Resources\ServiceRequestResource\Widgets\OrderServiceStats;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
@@ -37,7 +38,9 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
-                OrderStats::class
+                OrderStats::class,
+                OrderServiceStats::class,
+
                 // Widgets\AccountWidget::class,
                 // Widgets\FilamentInfoWidget::class,
             ])

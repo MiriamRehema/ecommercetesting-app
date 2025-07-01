f7c7386f162b0bac52d005f23404820a307dcfba
Miriam Rehema <rehemamiriam30@gmail.com>
2025-07-01 09:25:43 +0200

    created order service stats

--- app/Filament/Resources/OrderResource/Widgets/OrderStats.php
+++ app/Filament/Resources/OrderResource/Widgets/OrderStats.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Resources\OrderResource\Widgets;
 use App\Models\Order;
+use Filament\Support\Enums\IconPosition;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 use Illuminate\Support\Number;
@@ -12,11 +13,24 @@ class OrderStats extends BaseWidget
     protected function getStats(): array
     {
         return [
-           Stat::make('New Orders',Order::query()->where('status','new')->count()),
-           Stat::make('Order processing',Order::query()->where('status','processing')->count()),
-           Stat::make(' Order shipped',Order::query()->where('status','shipped')->count()),
-           Stat::make('Average Price',Number::currency(Order::query()->avg('grand_total')?? 0,'$'))
+           Stat::make('New Orders',Order::query()->where('status','new')->count())
+           ->chart([1,3,5,10,15,20,35,40])
+           ->color('success')
+           ->icon('heroicon-o-shopping-cart'),
+           Stat::make('Order procesing',Order::query()->where('status','procesing')->count())
+           ->chart([1,3,5,10,15,20,35,40])
+           ->color('warning')
+           ->icon('heroicon-o-cog'),
+           Stat::make(' Order shipped',Order::query()->where('status','shipped')->count())
+           ->chart([1,3,5,10,15,20,35,40])
+           ->color('info')
+           ->icon('heroicon-o-truck',IconPosition::Before),
+           Stat::make('Average Price',Number::currency(Order::query()->avg('grand_total')), '$')
+            ->chart([1,3,5,10,15,20,35,40])  
+           ->color('danger')    
+           ->icon('heroicon-o-currency-dollar'),
            
         ];
     }
+
 }

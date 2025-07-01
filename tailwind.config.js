b5de7bae0719880f743703652aad337c517041ab
Miriam Rehema <rehemamiriam30@gmail.com>
2025-07-01 14:08:25 +0200

    added tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./app/Filament/**/*.php",
+    "./resources/views/**/*.blade.php",
+    "./resources/js/**/*.vue",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
